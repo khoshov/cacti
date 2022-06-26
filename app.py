feba3a7b6b559c8a3355b0821485337520c2660c
Stanislav Khoshov <stanislav.khoshov@ubex.com>
2022-06-26 12:58:41 +0400

    some updates

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, jsonify, render_template, request, url_for
 from flask_admin import Admin, form
 from flask_admin.contrib.sqla import ModelView
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from markupsafe import Markup
-from sqlalchemy_utils import ChoiceType
+from sqlalchemy_utils import ChoiceType, generic_relationship
 from werkzeug.exceptions import NotFound
 from wtforms import TextAreaField
 from wtforms.widgets import TextArea
@@ -45,6 +45,16 @@ class Cactus(db.Model):
 
     def __repr__(self):
         return '<Cactus %r>' % self.name
+
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    object_type = db.Column(db.Unicode(255))
+    object_id = db.Column(db.Integer)
+    object = generic_relationship(object_type, object_id)
+
+    def __repr__(self):
+        return '<Like %r>' % self.name
 
 
 class RelatedProduct(db.Model):
@@ -128,6 +138,41 @@ def detail(pk):
     if not cactus:
         raise NotFound
     return render_template('detail.html', cactus=cactus)
+
+
+@app.route('/api/likes/cactus/<int:pk>')
+def cactus_likes(pk):
+    cactus = Cactus.query.filter_by(id=pk).first()
+    likes = Like.query.filter_by(object=cactus).count()
+    return jsonify({'likes': likes})
+
+
+@app.route('/api/likes/cactus', methods=['POST'])
+def create_cactus_likes():
+    if request.method == 'POST':
+        data = request.json
+        pk = data.get('pk')
+        cactus = Cactus.query.filter_by(id=pk).first()
+        like = Like(object=cactus)
+        db.session.add(like)
+        db.session.commit()
+        return jsonify({'detail': 'liked'})
+
+
+# @app.route('/likes/product/<int:pk>')
+# def detail(pk):
+#     cactus = Cactus.query.filter_by(id=pk).first()
+#     if not cactus:
+#         raise NotFound
+#     return jsonify()
+
+
+# @app.route('/like/cactus', methods=['POST'])
+# def user():
+#     if request.method == 'POST':
+#         data = request.form
+#         print(data)
+#         return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
